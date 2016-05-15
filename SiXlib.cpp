cc53fba3a3647b51eb8910aa158f0695591ff6e8
Shiichi <t448765@gmail.com>
2016-05-15 14:14:55 -0500

    -20160516-
    剩曲率判斷要弄，Keywords已編輯。

--- SiXlib.cpp
+++ SiXlib.cpp
@@ -10,13 +10,16 @@ SiXlib::SiX(void)
 {
 }
 
-void SiXlib::SetPID(int MOD,double MAXval,double MINval)
+double MOD,MAXval,MINval;
+void SiXlib::SetPID(int MOD,double Aval,double Bval)
 {
 //設定輸入訊號是否去小數點
 	if (MOD > 0)
 	 MOD = 1; //是
 	else
 	 MOD = 0; //否
+	MAXval = Aval;
+	MINval = Bval;
 }
 
 double interror,olderror;
@@ -24,15 +27,78 @@ void SiXlib::LinePID(double intput,double KP,double KI,double KD)
 {
 	if (MOD == 1)
 	  intput = ((int)intput);
-
     double error = intput;
     interror = (error * KI) + interror;
     double lasterror = (error * KD) - olderror;
     olderror = error;
-    intput = (error + interror + lasterror) * KP;
+    double output = (error + interror + lasterror) * KP;
 
     if (intput > MAXval)
       intput = MAXval;
     if (intput < MINval)
       intput = MINval;
+    return output;
+}
+
+void SiXlib::Timer(int start,unsigned long timekey)
+{
+  static unsigned long time = start;
+  time += timekey;
+  return time;
+}
+
+int Pin1A, Pin1B, Pin2A, Pin2B;
+void SiXlib::SetMotoPin(int P1A,int P1B,int P2A,int P2B)
+{
+	Pin1A = P1A;
+	Pin1B = P1B;
+	Pin2A = P2A;
+	Pin2B = P2B;
+}
+
+void SiXlib::Speed(int Left,int Right)
+{
+  if (Left > 0) {
+    analogWrite(Pin1A, Left);
+    analogWrite(Pin1B, 0);
+  }
+  else {
+    analogWrite(Pin1A, 0);
+    analogWrite(Pin1B, -Left);
+  }
+  if (B > 0) {
+    analogWrite(Pin2A, Right);
+    analogWrite(Pin2B, 0);
+  }
+  else {
+    analogWrite(Pin2A, 0);
+    analogWrite(Pin2B, -Right);
+  }
+}
+
+void SiXlib::TimeTurn(int time,int Left,int Right)
+{
+	while(1)
+	{
+		Speed(Left, Right);
+	    static unsigned long ENDt = Timer(0,1);
+		if ((ENDt/(time*1000)) = 1)
+		  break;
+	}
+}
+
+double OldCorrectVal;
+void SiXlib::WheelDiffer(double CorrectVal,int Frequency)
+{
+	if (Frequency <= 0)
+	 Frequency = 1;
+	if (Frequency >100)
+	 Frequency = 100;
+	for (int i; i > Frequency; i++)
+	{
+		if (abs(CorrectVal) < abs(OldCorrectVal))
+		  CorrectVal = OldCorrectVal;
+	  	OldCorrectVal = CorrectVal;
+	}
+	return CorrectVal;
 }
--- SiXlib.h
+++ SiXlib.h
@@ -9,8 +9,25 @@
 
 class SiX
 {
- void LinePID(double intput,double KP,double KI,double KD);
+//功能一: PID
  void SetPID(int MOD,double MAXval,double MINval);
+ void LinePID(double intput,double KP,double KI,double KD);
+
+//功能二: 計時器
+ void Timer(int start,unsigned long timekey);
+
+//功能三: 正負值馬達控制
+ void SetMotoPin(int Pin1A,int Pin1B,int Pin2A,int Pin2B);
+ void Speed(int Left,int Right);
+
+//功能四: 限時轉彎
+ void TimeTurn(int time,int Left,int Right);
+
+//功能五: 曲率判斷
+ void Curvature(double LeftSP,double RightSP);
+
+//功能六: 輪差修正
+ void WheelDiffer(double CorrectVal,int Frequency);
 
 };
 
